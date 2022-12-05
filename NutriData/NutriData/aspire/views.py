18c82e06c3064d1bbf6746072471b2d04bd5c846
McKenna Staley <mnnorton@byu.edu>
2022-12-05 12:01:58 -0700

    more changes

--- NutriData/NutriData/aspire/views.py
+++ NutriData/NutriData/aspire/views.py
@@ -52,18 +52,6 @@ def savedPageView(request, user_id=1, recipe_name=None, ingredient_name=None, in
     else :
         recipe_dict = dict()
 
-    #search ingredients
-    if ingredient_name != None :
-        ingredient_dict = searchIngredients(ingredient_name)
-    else :
-        ingredient_dict = dict()
-
-    #enter measurement for ingredients
-    if ingredient_id != None :
-        measure_list = getIngredientInformation1(ingredient_id)
-    else :
-        measure_list = list()
-
     user = User.objects.get(id = user_id)
     meal_dict = Recipe_User.objects.filter(user = user_id)
 
@@ -76,9 +64,6 @@ def savedPageView(request, user_id=1, recipe_name=None, ingredient_name=None, in
             'user' : user,
             'ingredient_name' : ingredient_name,
             'recipe_dict' : recipe_dict,
-            'ingredient_dict' : ingredient_dict,
-            'ingredient_id' : ingredient_id,
-            'measure_list' : measure_list,
         }
 
     return render(request, 'health_app/dash.html', context)
